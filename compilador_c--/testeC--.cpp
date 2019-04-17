f2b3a36067bf2b94cb24170c661b42c0d817d039
Gabriel <gabrielmatos.comp@gmail.com>
2019-04-16 22:37:35 -0300

    testes inseridos

--- compilador_c--/testeC--.cpp
+++ compilador_c--/testeC--.cpp
@@ -1,10 +1,10 @@
 programa
 
-int soma(int a, int b){
+int soma(int a, int b){     /*funcao que soma dois numeros*/
     return a + b;
 }
 
-int maior(float a, float b){
+int maior(float a, float b){ /*funcao que retorna o maior valor entre 2*/
     if(a >= b){
         return a;
     }
@@ -15,11 +15,13 @@ int maior(float a, float b){
 
 int main(){
     int x = 0;
-    int y = 3;
+    int y = 4;
+    int vet[5];
     int soma = 0;
-    while(x <= y){
-        soma = soma + maior(x, y);
+    while(x <= 4){
+        vet[x] = maior(x, y);
         x = x + 1;
+
     }
     return 0;
 }
